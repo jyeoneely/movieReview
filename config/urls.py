06354b4e27b56e103a8fc83e526be2386246285a
hong0108 <tjrdn960108@gmail.com>
2021-07-29 10:26:25 +0900

    홍석우 앱추가

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from movie import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
-    path('movie/', views.index),
+    path('movie/', include('movie.urls')),
     path('master/', include('master.urls')),
 ]
