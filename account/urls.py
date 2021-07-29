434e1a7b6f1df71ee4f241e289a27c5ec43645a2
jyeon <0927jy20@gmail.com>
2021-07-30 02:35:33 +0900

    로그인, 회원가입 html 생성

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,7 @@ from account import views
 
 app_name = 'account'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login
+from account.forms import UserForm
 
 # Create your views here.
 def index(request) :
     return HttpResponse("hello site")
+
+
+def signup(request):
+    """
+    회원가입
+    """
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
+            return redirect('index')
+    else:
+        form = UserForm()
+    return render(request, 'account/signup.html', {'form': form})
