1d5aa2838c8d7fa757be2e836a7b80ec0fe5c28a
jyeoneely <86949874+jyeoneely@users.noreply.github.com>
2021-08-02 01:02:08 +0900

    migrate error, db삭제 후 재적용

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,19 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from account import views
+"""config URL Configuration
 
-app_name = 'account'
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+"""
+from django.urls import path
+from . import views
+
+app_name = "account"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('profile/<int:pk>', views.profile, name='profile')
+    path('', views.index),
 ]
