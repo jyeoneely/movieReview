070d1dbb6bbc225ebeab81b21062c4e12dd8cae1
jyeoneely <86949874+jyeoneely@users.noreply.github.com>
2021-08-02 00:37:40 +0900

    account 구현중

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/<int:pk>', views.profile, name='profile')
 ]
--- account/views.py
+++ account/views.py
@@ -1,27 +1,42 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
-from django.contrib.auth import authenticate, login
-from account.forms import UserForm
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
 
 # Create your views here.
 def index(request) :
-    return HttpResponse("hello site")
+    return render(request, 'account/index.html')
 
 
 def signup(request):
     """
     회원가입
     """
-    if request.method == "POST":
-        form = UserForm(request.POST)
+    if request.user.is_authenticated:   # 로그인 된 사용자의 경우 회원가입으로 이동하지 못하도록
+        return redirect('index')
+    if request.method == "POST":        # post로 요청이 들어올 경우 검증 후 index로 redirect
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)  # 사용자 인증
             login(request, user)  # 로그인
             return redirect('index')
     else:
-        form = UserForm()
+        form = UserCreationForm()       # get으로 요청이 들어올 경우 form 호출
     return render(request, 'account/signup.html', {'form': form})
+
+
+def profile(request, pk):
+    """
+    내 프로필
+    """
+    User = get_user_model()
+    user = get_object_or_404(User, pk=pk)
+    return render(request, 'account/profile.html', {'user' : user})
+
