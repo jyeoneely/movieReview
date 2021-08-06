e95985230251df9cd4e19999f2b5f61f6a7b6cf3
Seongil <ehssk3239@gmail.com>
2021-08-06 17:53:24 +0900

    게시글 페이지

--- community/views.py
+++ community/views.py
@@ -12,21 +12,19 @@ from community.models import *
 # Create your views here.
 # 글 목록
 def index(request):
-    boards = {'boards': Board.objects.order_by('-created_date')}
-    return render(request, 'community/board_list.html', boards)
+    # boards = {'boards': Board.objects.order_by('-created_date')}
+    # return render(request, 'community/board_list.html', boards)
 
- # # 입력 파라미터
- #    page = request.GET.get('page', '1')  # 페이지
- #
- #    # 조회
- #    review_list = Review.objects.order_by('-create_date')
- #
- #    # 페이징처리
- #    paginator = Paginator(review_list, 10)  # 페이지당 10개씩 보여주기
- #    page_obj = paginator.get_page(page)
- #
- #    context = {'review_list': page_obj}
- #    return render(request, 'review/review_list.html', context)
+    page = request.GET.get('page', '1')
+    # 조회
+    board_list = Board.objects.order_by('-created_date')
+
+    # 페이징처리
+    paginator = Paginator(board_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'board_list': page_obj}
+    return render(request, 'community/board_list.html', context)
 
 
 # 글 작성
@@ -35,7 +33,8 @@ def board_post(request):
         form = BoardForm(request.POST)
         if form.is_valid():
             board = form.save(commit=False)
-            board.create_date = timezone.now()
+            board.writer_id = 1
+            board.created_date = timezone.now()
             board.save()
             return redirect('community:index')
     else:
@@ -45,10 +44,22 @@ def board_post(request):
 
 # 글 상세보기
 def board_detail(request, post_id):
-    board = get_object_or_404(Board, id=post_id)
+    boardPerPage = Board.objects.order_by('-created_date')
 
-    board_after = get_object_or_404(Board, id=post_id+1)
-    board_before= get_object_or_404(Board, id=post_id-1)
+    index = -1
+    board_before = None
+    board_after = None
+
+    for b in boardPerPage:
+        index += 1
+        if b.id == post_id:
+            board = b
+            break
+    if index != 0:
+        board_before = boardPerPage[index - 1]
+    if index != -1:
+        board_after = boardPerPage[index + 1]
+
     context = {
         'board':board,
         'board_after':board_after,
@@ -72,7 +83,7 @@ def board_modify(request, post_id):
         form = BoardForm(request.POST, instance=board)
         if form.is_valid():
             board = form.save(commit=False)
-            board.modify_date = timezone.now()
+            board.modified_date = timezone.now()
             board.save()
             return redirect('community:detail', post_id=post_id)
     else:
